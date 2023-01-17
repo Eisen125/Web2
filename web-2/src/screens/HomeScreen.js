759a2bb1864f3a1c6b8440f4ae7ad3267f5fc62f
ido eisenberg <idoeisen125125@gmail.com>
2023-01-17 11:27:15 +0200

    Project-frame

---
+++ web-2/src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+
+
+
+
+
+
+export const HomeScreen = ()=>{
+    const fakedata =fetch('my-app/public/products.json')
+        console.log(fakedata)
+return(
+    <div>
+
+  <h1>Shosi shoes</h1>
+   
+    
+    </div>
+    )
+
+
+}
