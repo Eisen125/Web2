84e7c0738107b353385f0ae30c618b584ae69944
yuvalkatzir <42411319+yuvalkatzir@users.noreply.github.com>
2023-03-17 15:34:26 +0200

    delete, reduce, remove - order

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -54,14 +54,61 @@ export const CreateNewOrder = async (req, res) => {
   }
 };
 
-export const DeleteOrder = async (req, res) => {
-  let { userId, cartItem } = req.body;
-  const order = await Order.findOne(userId, cartItem)
+// lower order quantity
+export const ReduceQuantity = async (req, res) => {
+  let { userId } = req.body;
+  const user = await User.findOne({ fireBaseId: userId });
+  const order = await Order.findOne({ user: user._id });
+
   if (order) {
+    existingOrder.orderItems.forEach(item => {
+      if (item.id == cartItem.id && item.quantity !== 1) {
+        item.quantity = Math.max(1, item.quantity - 1);
+      }
+    });
+
+    const updatedOrder = await existingOrder.save();
+    res.status(201).json(updatedOrder);
+  } else {
+    res.status(404).send({ message: 'Order Not Found' });
+  }
+}
+
+// delete all items ordered
+export const DeleteOrder = async (req, res) => {
+  let { userId } = req.body;
+  const user = await User.findOne({ fireBaseId: userId });
+  const order = await Order.findOne({ user: user._id });
+  if (order !== null) {
     await order.remove();
-    res.send({ message: "order deleted" })
+    res.send({ message: "Order Deleted" })
   }
   else {
-    res.status(404).send({ message: 'order Not Found' });
+    res.status(404).send({ message: 'Order Not Found' });
+  }
+}
+
+// delete single item from order
+export const DeleteOrderItem = async (req, res) => {
+  let { userId, cartItem } = req.body;
+  const user = await User.findOne({ fireBaseId: userId });
+  const order = await Order.findOne({ user: user._id });
+
+  if (order) {
+    let removeItem = null;
+    order.orderItems.forEach(item => {
+      removeItem = (item.id == cartItem.id) ? item : null;
+    });
+    if (removeItem) {
+      order.orderItems.remove(removeItem);
+      const updatedOrder = await order.save();
+      res.status(201).json(updatedOrder);
+      res.send({ message: "Ordered Item Deleted" })
+    } else {
+      res.status(404).send({ message: 'Ordered Item Not Found' });
+    } 
+  }
+  else {
+    res.status(404).send({ message: 'Order Not Found' });
   }
 }
